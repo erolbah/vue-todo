7123acd30f9189219bae4e70275057b61d3bf92e
Erol Bakir <erol@2jours.nl>
2020-01-08 08:15:30 +0100

    init commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,189 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Hi</h1>
+    <input type="text" 
+      class="todo-input" 
+      placeholder="Voer handmatig checklist item in" 
+      v-model="newTodo" 
+      @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <div v-if="!todo.editing" 
+          @dblclick="editTodo(todo)" 
+          class="todo-item-label">
+            {{todo.title}}</div>
+        <input v-else 
+          class="todo-item-edit" 
+          type="text" 
+          v-model="todo.title" 
+          @blur="doneEdit(todo)" 
+          @keyup.enter="doneEdit(todo)" 
+          @keyup.esc="cancelEdit(todo)" 
+          v-focus>
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'Todo-list',
+  data () {
+    return {
+      newTodo: '',
+      beforeEditCache: '',
+      idForTodo: 4, 
+      todos: [
+        {
+          id: 1,
+          title: 'Afvoer controleren op afvoercapaciteit',
+          description: 'Deze afvoer werkt niet correct en zal opnieuw aangelegt moeten worden',
+          completed: false,
+          editing: false
+        },
+        {
+          id: 2,
+          title: 'Oplevering glaswerk',
+          description: 'Deze afvoer werkt niet correct en zal opnieuw aangelegt moeten worden',
+          completed: false,
+          editing: false
+        },
+        {
+          id: 3,
+          title: 'Beschadigingen op kozijnwerk controleren',
+          description: 'Deze afvoer werkt niet correct en zal opnieuw aangelegt moeten worden',
+          completed: false,
+          editing: false
+        },
+      ]
+    }
+  },
+  directives: {
+    focus: {
+      // directive definition
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+  methods: {
+    addTodo () {
+      if(this.newTodo.length === 0){
+        return
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        description: this.newTodo,
+        completed: false
+      })
+
+      this.newTodo = ''
+      this.idForTodo++
+    },
+    removeTodo(index){
+      this.todos.splice(index, 1)
+    },
+    editTodo(todo) {
+      this.beforeEditCache = todo.title
+      todo.editing = true
+    },
+    doneEdit(todo){
+      if(todo.title.trim() === ''){
+        todo.title = this.beforeEditCache
+      }
+      todo.editing = false
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache
+      todo.editing = false
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    animation-duration: 0.3s;
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
+    }
+  }
+  .todo-item-left { // later
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc; //override defaults
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    &:focus {
+      outline: none;
+    }
+  }
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+    &:hover {
+      background: lightgreen;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  .active {
+    background: lightgreen;
+  }
+  // CSS Transitions
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
 </style>
